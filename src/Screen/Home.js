ae0c4890f2a0a66470963c7f3cffd76aac80bed5
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-03-17 06:20:16 +0600

    ok

---
+++ src/Screen/Home.js
@@ -0,0 +1,24 @@
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+      <Text style={styles.text}>Hello, world!</Text>
+      
+      
+    </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop:100,
+  },
+  text: {
+    fontSize: 20,
+    color: 'red',
+  },
+});
