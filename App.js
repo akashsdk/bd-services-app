a83c1b8624315a5779f4d4df360e5f95e382d626
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-03-18 02:57:52 +0600

    ok

--- App.js
+++ App.js
@@ -1,67 +1,22 @@
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import HomeScreen from './src/Screen/Home';
-import MenuScreen from './src/Screen/Menu';
-import ServicesScreen from './src/Screen/Services';
-import ProfileScreen from './src/Screen/Profile';
-import LogIn from './src/Screen/LogIn';
-
-const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
-
-const MainStack = () => (
-  <Tab.Navigator
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
-
-        if (route.name === 'Home') {
-          iconName = focused ? 'home' : 'home-outline';
-        } else if (route.name === 'Menu') {
-          iconName = focused ? 'menu' : 'menu-outline';
-        } else if (route.name === 'Services') {
-          iconName = focused ? 'list' : 'list-outline';
-        } else if (route.name === 'Profile') {
-          iconName = focused ? 'person' : 'person-outline';
-        }
-
-        return <Ionicons name={iconName} size={size} color={color} />;
-      },
-    })}
-    tabBarOptions={{
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    }}
-  >
-    <Tab.Screen name="Home" component={HomeScreen} />
-    <Tab.Screen name="Menu" component={MenuScreen} />
-    <Tab.Screen name="Services" component={ServicesScreen} />
-    <Tab.Screen name="Profile" component={ProfileScreen} />
-  </Tab.Navigator>
-);
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React from 'react'
 
 export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    // Perform login logic here
-    setIsLoggedIn(true); // Update the isLoggedIn state upon successful login
-  };
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {isLoggedIn ? (
-          <Stack.Screen name="Main" component={MainStack} />
-        ) : (
-          <Stack.Screen name="LogIn" options={{ headerShown: false }}>
-            {({ navigation }) => <LogIn navigation={navigation} onLogin={handleLogin} />}
-          </Stack.Screen>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    <ScrollView>
+      <View style={styles.container}>
+      <Text style={styles.text}>Hello, world! ok ok </Text>
+    </View>
+    </ScrollView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop:100,
+  },
+  text: {
+    fontSize: 20,
+    color: 'red',
+  },
+});
--- src/Screen/Profile.js
+++ src/Screen/Profile.js
@@ -1,39 +1,10 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { View, Text } from 'react-native'
+import React from 'react'
 
 export default function Profile() {
-  const navigation = useNavigation();
-
-  const handleLogout = () => {
-    // Show an alert
-    Alert.alert(
-      'Logout',
-      'Are you sure you want to log out?',
-      [
-        {
-          text: 'Cancel',
-          style: 'cancel',
-        },
-        {
-          text: 'OK',
-          onPress: () => {
-            // Perform logout logic
-            // For now, let's assume logout is successful and navigate to the Login screen
-            navigation.navigate('LogIn');
-          },
-        },
-      ],
-      { cancelable: false }
-    );
-  };
-
   return (
     <View>
       <Text>Profile</Text>
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>LogOut</Text>
-      </TouchableOpacity>
     </View>
-  );
+  )
 }
